cf7732b844d6794195ecc2d444ecc1e438bc35a1
FER-nanda06 <tmn23046@uicslp.edu.mx>
2025-03-29 15:33:37 -0600

    Add files via upload

---
+++ practica29.php
@@ -0,0 +1,42 @@
+<?php 
+//DADO UN NUMERO ENTERO, DEVOLVER EL DOBLE DEL 
+//NUMERO SI EL NUMERO ES POSITIVO, EL TRIPLE DEL NUMERO 
+//SI ES NEGATIVO, Y CERO SI EL NUMERO ES NEUTRO.
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRACTICA 29</title>
+</head>
+
+<body>
+    <header><h1>DOBLE Y TRIPLE</h1></header>
+    <main>
+        <section>
+            <form action="" method="POST">
+                <label for="num">Ingresa un numero</label>
+                <input type="number" name="num" id="num">
+                <button type="submit">Enviar</button>
+            </form>
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $numero = $_POST['num'];
+    if($numero >0){
+        $resultado =$numero * 2;
+}else if($numero < 0){
+    $resultado = $numero * 3;
+}else{
+    $resultado = 0;
+}
+echo "El resultado es: $resultado";
+}
+
+?>
+<a href="practica30.php">Siguiente Practica</a>
+ </section>
+    </main>
+    <footer><p>Maria Fernanda Gonzalez Pineda</p></footer>
+</body>
+</html
